30f82b4449fdb2be944a1649230fc3e31bd6f9ab
shahd <shahdandalgny123@gmail.com>
2026-09-19 21:05:01 +0300

    move files to src folder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv('assets/.env')  # Load environment variables from .env file
+from routes import base
 app = FastAPI()
+app.include_router(base.base_router)
 
-@app.get("/Welcome")
-def welcome_message():
-    return {"message": "Welcome to mini-RAG!"}
